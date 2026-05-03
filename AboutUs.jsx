0927ab841c6eec1c0b1adb440b3cfe257058827d
Amr <amroalnajjar34@gmail.com>
2026-05-03 13:05:21 +0400

    Add AboutUs component with company information

---
+++ AboutUs.jsx
@@ -0,0 +1,9 @@
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Our Company</h1>
+      <p>We are a tech company building great products.</p>
+    </div>
+  );
+}
+export default AboutUs;
